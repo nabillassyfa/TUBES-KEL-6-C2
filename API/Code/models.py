d7336c77355fac91d0690f45bb822b84567bd545
Wildan Raffians <wildanhraffianshar@gmail.com>
2024-05-24 14:29:51 +0700

    fix webservice

--- API/Code/models.py
+++ API/Code/models.py
@@ -17,19 +17,19 @@ class RS(BaseDB):
 
     jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="RS")
     
-    DataRS = relationship("DaftarRS", back_populates="Rs")
-    jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="rs")
+    daftarRS = relationship("DaftarRS", back_populates="RS")
+    jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="RS")
     
 class Dokter(BaseDB):
     __tablename__ = "dokter"
     id = Column(Integer, primary_key=True)
     nama = Column(String, index=True)
-    spesialis = Column(Integer, ForeignKey('spesialis.id'))
+    id_spesialis = Column(Integer, ForeignKey('spesialis.id'))
     foto = Column(String, index=True)
     informasi = Column(String, nullable=False)
     pengalaman = Column(Integer)
     
-    spesialis_ref = relationship("Spesialis", back_populates="dokter")
+    spesialis = relationship("Spesialis", back_populates="dokter")
     rekam_medis = relationship("RekamMedis", back_populates="dokter")
     rating = relationship("Rating", back_populates="dokter")
     jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="dokter")
@@ -54,14 +54,10 @@ class Spesialis(BaseDB):
     spesialis = Column(String, index=True)
     icon = Column(String, index=True)
     
-    dokter = relationship("Dokter", back_populates="spesialis_ref")
-<<<<<<< HEAD
-    dataSpesialis = relationship("DaftarRS", back_populates="Spesialis")
+    dokter = relationship("Dokter", back_populates="spesialis")
     jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="spesialis")
-=======
-    jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="spesialis_ref")
+    daftarRS = relationship("DaftarRS", back_populates="spesialis")
 
->>>>>>> 753958fad54f30ae62234f4499d50b6121f06ade
        
 class Rating(BaseDB):
     __tablename__ = "rating"
@@ -95,8 +91,8 @@ class DaftarRS(BaseDB):
     id_spesialis = Column(Integer, ForeignKey('spesialis.id'))
     id_RS = Column(Integer, ForeignKey('RS.id'))
     
-    Spesialis = relationship("Spesialis", back_populates="dataSpesialis")
-    Rs = relationship("RS", back_populates="DataRS")
+    spesialis = relationship("Spesialis", back_populates="daftarRS")
+    RS = relationship("RS", back_populates="daftarRS")
     
 
 class Artikel(BaseDB):
@@ -125,10 +121,6 @@ class User(BaseDB):
     infoUser = relationship("InfoUser", back_populates="user")
     jadwal_janji_temu = relationship("JadwalJanjiTemu", back_populates="user")
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 753958fad54f30ae62234f4499d50b6121f06ade
 class JadwalJanjiTemu(BaseDB):
     __tablename__ = "jadwal_janji_temu"
     id = Column(Integer, primary_key=True)
@@ -142,5 +134,5 @@ class JadwalJanjiTemu(BaseDB):
 
     dokter = relationship("Dokter", back_populates="jadwal_janji_temu")
     user = relationship("User", back_populates="jadwal_janji_temu")
-    rs = relationship("RS", back_populates="jadwal_janji_temu")
+    RS = relationship("RS", back_populates="jadwal_janji_temu")
     spesialis = relationship("Spesialis", back_populates="jadwal_janji_temu")
